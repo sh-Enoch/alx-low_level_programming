6dff9d40302f9e8bda805b0f84fb8f2b17060c99
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-03-16 19:24:19 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char", sizeof(char));
+	printf("Size of an int\n", sizeof(int));
+	printf("Size of a long int\n", sizeof(long int));
+	printf("Size of a long long int\n", sizeof(long long float);
+	printf("Size of a float\n", sizeof(float));
+	return (0);
+}
