1f29800b9c5311a3c474d31aea1e38ad9664b22c
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-03-16 18:54:53 +0000

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - with proper grammer
+ * Return: always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
