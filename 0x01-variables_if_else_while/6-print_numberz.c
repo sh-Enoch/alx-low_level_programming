d09140faa13303597a09b36cf95f2c12d3861af8
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-03-17 20:05:26 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numberz
+ * Return: always 0
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar((y % 10) + '0');
+	}
+	putcha('\n');
+	return (0);
+}
