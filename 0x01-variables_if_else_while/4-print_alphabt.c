9c6de8a9f458ef00c0bffa7fc8a07382691e29e8
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-03-17 19:30:54 +0000

    no e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabet except e and q
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alph[24] = "abcdfghijklmnoprstuvwxyz";
+	int x;
+
+
+	for (x = 0; x < 24; x++)
+	{
+		putchar(alph[x]);
+	}
+	putchar("\n");
+	return (0);
+}
