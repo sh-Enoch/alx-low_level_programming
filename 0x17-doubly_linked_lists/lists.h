0400027eb111f1e9f1b692037a6520fc298ebf30
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-24 06:22:41 +0000

    get node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -21,6 +21,8 @@ typedef struct dlistint_s
 } dlistint_t;
 
 
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
 size_t print_dlistint(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
